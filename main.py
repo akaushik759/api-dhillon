d22a965eb54cca9f7e1610addbcca3136c42bfc8
Abhishek Kaushik <kaushikdeveloper759@gmail.com>
2022-03-07 01:53:57 +0530

    made default route without lyrics and /lyrics route for json with lyrics

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from dhillon import dhillon_json
+from dhillon_lyrics import dhillon_lyrics_json
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@ app.route('/', methods=['GET'])
 def index():
     return jsonify(dhillon_json)
+
+
+@ app.route('/lyrics', methods=['GET'])
+def with_lyrics():
+    return jsonify(dhillon_lyrics_json)
 
 
 # app.run(host='localhost', port=5000, debug=True)
